a364430785e5fc1690360ffa167d34a84124080f
Zhou <yimizhou@amd.com>
2023-06-15 20:05:49 +0800

    Add support to AMD's MIGraphX backend

---
+++ byte_mlperf/backends/MIGRAPHX/compile_backend_migraphx.py
@@ -0,0 +1,152 @@
+import os
+import json
+import logging
+import copy
+
+import torch
+# import lego
+
+import tensorflow as tf
+
+import onnx
+
+import migraphx
+
+tf.get_logger().setLevel('ERROR')
+
+import numpy as np
+import time
+from byte_mlperf.backends import compile_backend
+import subprocess
+
+log = logging.getLogger("CompileBackendMIGRAPHX")
+from byte_mlperf.tools import saved_to_onnx, torch_to_onnx
+
+pt_dtype_map = {
+    "FLOAT32": torch.float32,
+    "FLOAT16": torch.float16,
+    "INT8": torch.int8,
+    "INT32": torch.int32,
+    "LONG": torch.long,
+    "BOOL": torch.bool
+}
+
+tf_dtype_map = {
+    "FLOAT32": tf.float32,
+    "FLOAT16": tf.float16,
+    "INT32": tf.int32,
+}
+
+INPUT_TYPE = {
+    "UINT8": np.uint8,
+    "FLOAT32": np.float32,
+    "LONG": np.long,
+    "INT32": np.int32,
+    "INT64": np.int64,
+    "BOOL": np.bool,
+}
+
+
+class CompileBackendMIGRAPHX(compile_backend.CompileBackend):
+    def __init__(self):
+        super(CompileBackendMIGRAPHX, self).__init__()
+        self.target = 'rocm'
+        self.hardware_type= 'MIGRAPHX'
+
+    def compile(self, config, dataloader=None):
+        self.model_name = config['model_info']['model']
+        self.framework = config['model_info']['framework']
+        self.input_type = config['model_info']['input_type'].split(",")
+
+        model_paths = config['model_info']['model_path'].split('.')
+        model_path = config['model_info']['model_path']
+        test_data = dataloader.get_fake_samples(
+            1, config['model_info']['input_shape'], self.input_type)
+        input_tensors = []
+        #convert tf/pytorch model to onnx model, tf works, not verify pytorch now.
+        model_onnx_path=model_path
+        if self.framework == "Tensorflow":
+            model_onnx_path = os.path.join(model_path,self.model_name+ '.onnx')
+            if not os.path.exists(model_onnx_path):
+                saved_to_onnx.savedmodel_to_onnx(model_path, model_onnx_path)
+        elif self.framework == "Pytorch":
+            model_onnx_path = os.path.join(os.path.dirname(model_path), self.model_name + '.onnx')
+            if not os.path.exists(model_onnx_path):
+                saved_to_onnx.torch_to_onnx(model_path, str(model_onnx_path))
+        batch_sizes = config['workload']['batch_sizes']
+        for batch_size in batch_sizes:
+            print(batch_sizes)
+            model_path_for_batch_size = model_onnx_path.rsplit("/",1)
+            model_onnx_path_set_batch_size = os.path.join(model_path_for_batch_size[0],str(batch_size),model_path_for_batch_size[1])
+            model_dir = os.path.join(model_path_for_batch_size[0],str(batch_size))
+            if not os.path.exists(model_dir):
+                os.makedirs(model_dir)
+            model_paths = model_onnx_path_set_batch_size.split('.')
+            model_path = model_paths[0] + "_optimized." + model_paths[1]
+            if not os.path.exists(model_path):
+                new_input = {}
+                for key, value in config['model_info']['input_shape'].items():
+                    if(('videobert' in config['model_info']['model']) and (key=='text')):
+                        new_input[key]=value
+                    else:
+                        new_input[key]=[value[0]*batch_size]+value[1:]
+                model = migraphx.parse_onnx(model_onnx_path,map_input_dims=new_input,default_dim_value=batch_size)
+                model.compile(migraphx.get_target("gpu"))
+                migraphx.save(model, model_path, format='msgpack')
+
+        result = {
+            "model":
+            self.model_name,
+            "framework":
+            config['model_info']['framework'],
+            "compile_precision":
+            config['model_info']['model_precision'],
+            "input_type":
+            self.input_type,
+            "max_batch_size":
+            config['model_info']['max_batch_size'],
+            "compile_status":
+            "success",
+            "sg_percent":
+            100,
+            "segments": [
+                {
+                    "sg_idx":
+                    0,
+                    "is_fallback":
+                    False,
+                    "input_tensor_map":
+                    config['model_info']['input_shape'],
+                    "output_tensor_map":
+                    config['model_info']['outputs'],
+                    "compiled_model": [
+                        {
+                            "compiled_bs": config["workload"]["batch_sizes"][-1],
+                            "compiled_obj": model_path,
+                        },
+                    ],
+                },
+            ]
+        }
+        self.configs = result
+        self.workload = config['workload']
+        self.model_info = config['model_info']
+        return result
+
+    def get_interact_profile(self, config):
+        model_profile = []
+        file_path = "backends/MIGRAPHX/" + self.hardware_type + '.json'
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as f:
+                model_profile = json.load(f)
+        else:
+            log.info(
+                'File path: {} does not exist, please check'.format(file_path))
+
+        return model_profile
+
+    def get_best_batch_size(self):
+        """
+        Get Best Batch Size for the model
+        """
+        return None
---
+++ byte_mlperf/backends/MIGRAPHX/runtime_backend_migraphx.py
@@ -0,0 +1,236 @@
+import os
+import json
+import logging
+import copy
+
+import torch
+# import lego
+
+import tensorflow as tf
+
+import onnx
+# import onnx_tensorrt.backend as onnx_convert
+
+tf.get_logger().setLevel('ERROR')
+
+import numpy as np
+import time
+from byte_mlperf.backends import runtime_backend
+
+log = logging.getLogger("RuntimeBackendMIGRAPHX")
+
+pt_dtype_map = {
+    "FLOAT32": torch.float32,
+    "FLOAT16": torch.float16,
+    "INT8": torch.int8,
+    "INT32": torch.int32,
+    "LONG": torch.long,
+    "BOOL": torch.bool
+}
+
+tf_dtype_map = {
+    "FLOAT32": tf.float32,
+    "FLOAT16": tf.float16,
+    "INT32": tf.int32,
+}
+
+INPUT_TYPE = {
+    "UINT8": np.uint8,
+    "FLOAT32": np.float32,
+    "LONG": np.long,
+    "INT32": np.int32,
+    "INT64": np.int64,
+    "BOOL": np.bool,
+}
+
+class RuntimeBackendMIGRAPHX(runtime_backend.RuntimeBackend):
+    def __init__(self):
+        super(RuntimeBackendMIGRAPHX, self).__init__()
+        self.hardware_type = 'MIGRAPHX'
+        self.need_reload = False
+        self.model_runtimes = []
+        self.configs = None
+        self.batch_size = -1
+
+    def predict(self, feeds):
+        if not self.model_runtimes:
+            self._load()
+        if(self.old_batch_size != self.batch_size):
+            self.load()
+
+        results = {}
+        if self.framework == "Tensorflow" or self.framework == "Onnx":
+            params = {}
+            key_id = 0
+            for key in feeds.keys():
+                ###new_key = key.replace(":", "_")
+                if( ( 'layout' in self.model_info ) and ( self.model_info['layout'] == 'NHWC' ) and ( feeds[ key ].shape[3] != 3 ) ):
+                    params[ key ] = np.ascontiguousarray(np.transpose( feeds[ key ] , axes=[0, 2, 3, 1]))
+                elif( isinstance( feeds[key] , list ) ):
+                    params[ key ] = np.array( feeds[ key ] , dtype = INPUT_TYPE[ self.input_type[key_id] ] )
+                else:
+                    params[ key ] = feeds[ key ]
+                key_id += 1
+
+
+            results = {}
+            for model_runtime in self.model_runtimes:
+                _results = model_runtime.run( params )
+                if( 'videobert' in self.configs['model'] ):
+                    if( isinstance( _results , dict ) ):
+                        assert( ( len( _results.keys() ) == 1 ) and ( 'output' in _results.keys() ) )
+                        assert( ( len( _results[ 'output' ][0] ) % self.batch_size ) == 0 )
+                        results = np.array( _results[ 'output' ] ).reshape( self.batch_size , len( _results[ 'output' ] ) * len( _results[ 'output' ][0] ) // self.batch_size )
+                    else:
+                        results = np.array( _results[0] )
+                        results = results.reshape( self.batch_size , results.shape[0] * results.shape[1] // self.batch_size )
+                    results = [ results ]
+                else:
+                    if( len( self.outputs ) == 1 ):
+                        results[ self.outputs[0] ] = np.array( _results[0].tolist() ).reshape( _results[0].get_shape().lens() )
+                    else:
+                        for i in range( len( self.outputs ) ):
+                            results[ self.outputs[i] ] = [ np.array( _results[i] ).reshape( _results[i].get_shape().lens() ) ]
+            assert len(results) != 0
+
+        elif self.framework == "Pytorch":
+            input_tensors = []
+            i = 0
+            for key, _ in feeds.items():
+                input_tensors.append(
+                    torch.tensor(feeds[key],
+                                 dtype=pt_dtype_map[self.input_type[i]]).to(
+                                     self.device))
+                i += 1
+            with torch.no_grad():
+                for model_runtime in self.model_runtimes:
+                    results = model_runtime(*input_tensors)
+
+            if isinstance(results, dict):
+                for key, val in results.items():
+                    results[key] = val.cpu().detach().numpy()
+            elif isinstance(results, tuple):
+                dic = {}
+                for i, key in enumerate(self.outputs):
+                    dic[key] = list(results)[i]
+            else:
+                results = {self.outputs[0]: results.cpu().numpy()}
+
+
+        else:
+            for model_runtime in self.model_runtimes:
+                results = model_runtime.run(feeds)
+        ##### Modified by Ngaiman Chow on 2023-6-13 for adapting all the code into MIGraphX runtime backend
+        ###return results
+        if( 'videobert' in self.configs['model'] ):
+            return results, {}
+        else:
+            return results
+        ##### Modified by Ngaiman Chow on 2023-6-13 for adapting all the code into MIGraphX runtime backend
+
+    def benchmark(self, dataloader):
+        batch_sizes = self.workload['batch_sizes']
+        if( self.batch_size is not None ):
+            batch_size = self.batch_size
+        else:
+            batch_size = self.workload[ 'batch_sizes' ][ 0 ]
+        iterations = self.workload['iterations']
+        reports = []
+        if True:
+            times_range = []
+            report = {}
+            report['BS'] = batch_size
+            test_data = dataloader.get_fake_samples(
+                batch_size, self.configs['segments'][0]['input_tensor_map'],
+                self.configs['input_type'])
+
+            for _ in range(30):
+                self.predict(test_data)
+
+            for _ in range(iterations):
+                start_time = time.time()
+                self.predict(test_data)
+                end_time = time.time()
+                times_range.append(end_time - start_time)
+
+            times_range.sort()
+            tail_latency = round(
+                times_range[int(len(times_range) * 0.99)] * 1000, 2)
+            avg_latency = round(sum(times_range) / iterations * 1000, 2)
+            qps = int(1000.0 * batch_size / avg_latency)
+
+            report['QPS'] = qps
+            report['AVG Latency'] = avg_latency
+            report['P99 Latency'] = tail_latency
+            reports.append(report)
+
+        return reports
+
+    def get_loaded_batch_size(self):
+        return self.batch_size
+
+    def _load(self):
+        for i, segment in enumerate(self.configs['segments']):
+            # there is no input/output meta data i the graph so it need to come from config.
+            if not segment['input_tensor_map']:
+                raise ValueError("Segment " + str(i) + " needs inputs")
+            if not segment['output_tensor_map']:
+                raise ValueError("Segment " + str(i) + " needs outputs")
+
+            self.input_shapes = segment['input_tensor_map']
+            self.outputs = segment['output_tensor_map'].split(",")
+
+            if( self.framework == "Tensorflow" or self.framework == "Onnx"):
+                onnx_file_path = segment['compiled_model'][0]['compiled_obj']
+                import migraphx
+                onnx_file_path_for_batch_size = onnx_file_path.rsplit("/",2)
+                onnx_file_path=os.path.join(onnx_file_path_for_batch_size[0],str(self.batch_size),onnx_file_path_for_batch_size[-1])
+                model=migraphx.load(onnx_file_path,format='msgpack')
+            elif self.framework == "Pytorch":
+                self.device = "cuda" if torch.cuda.is_available() else "cpu"
+                model = torch.jit.load(
+                    segment['compiled_model'][0]['compiled_obj'],
+                    torch.device(self.device))
+                if torch.cuda.is_available():
+                    model.cuda()
+                inputs = list(model.graph.inputs())
+                names = [i.debugName() for i in inputs]
+                model.eval()
+            else:
+                # original_model = onnx.load(segment['compiled_model'][0]['compiled_obj'])
+                # model =  onnx_convert.prepare(original_model, device='CUDA:1')
+
+                import onnxruntime
+                model = onnxruntime.InferenceSession(
+                    segment['compiled_model'][0]['compiled_obj'],
+                    providers=['ROCmExecutionProvider'])
+
+            self.model_runtimes.append(model)
+
+    def _get_fake_samples(self, batch_size, shape, input_type):
+        data = {}
+        if input_type:
+            i = 0
+            for key, val in shape.items():
+                if key != "text":
+                    val = [val[0] * batch_size] + val[1:]
+                    data[key] = np.random.random(size=val).astype(
+                        INPUT_TYPE[input_type[i]])
+                else:
+                    data[key] = np.random.random(size=val).astype(
+                        INPUT_TYPE[input_type[i]])
+                i += 1
+            return data
+        else:
+            raise ValueError("Please provide input type")
+
+    def load(self, batch_size = 1) -> None:
+        self.batch_size = batch_size
+        self.model_runtimes = []
+        self.input_type = self.configs['input_type']
+        self.framework = self.configs['framework']
+
+        self.model_name = self.configs['model']
+        
+        self._load()
+        self.old_batch_size = batch_size
