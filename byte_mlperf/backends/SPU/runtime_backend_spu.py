3ebe2926f55b9c03a6b9e2c15f349d123cd431c6
moffett-ai <ian.yan@moffett.ai>
2023-07-14 19:30:10 +0800

    remove data dependent processing.

--- byte_mlperf/backends/SPU/runtime_backend_spu.py
+++ byte_mlperf/backends/SPU/runtime_backend_spu.py
@@ -7,8 +7,7 @@ import yaml
 import multiprocessing
 from multiprocessing import Manager
 from byte_mlperf.backends import runtime_backend
-import spu_backend
-from inference import SQUADTester, ImageNetTester, ConformerTester, get_yaml_path, squad_preprocess, squad_postprocess
+from inference import ModelFactory
 
 hardware_type = "spu".upper()
 
@@ -50,7 +49,6 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
         self.workload = None
         self.model_info = None
         self.model = None
-        self.yaml_config = None
         self.need_reload = True
 
     def predict(self, feeds):
@@ -66,9 +64,9 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
             response = self.model.inference(request)
             return response
         elif self.model_name in ["bert-torch-fp32", "albert-torch-fp32", "roberta-torch-fp32"]:
-            request, model_info = squad_preprocess(feeds, self.yaml_config)
+            request, info = self.model.preprocess(feeds, self.model_info)
             response = self.model.inference(request)
-            response = squad_postprocess(response, model_info)
+            response = self.model.postprocess(response, info)
             return response
         else:
             raise NotImplementedError(f"task: {self.model_name} not supported")
@@ -80,22 +78,22 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
         for idx, batch_size in enumerate(batch_sizes):
             if batch_size != self.batch_size:
                 continue
-            self.yaml_config.update(
-                {"min_batch_size": self.yaml_config['chunk_size']})
+            self.model_info.update(
+                {"min_batch_size": self.model_info['chunk_size']})
             report = {}
             dataloader.rebatch(batch_size)
             if self.model_name == "resnet50-torch-fp32":
                 test_data, _ = dataloader.get_samples(0)
                 all_resnet50_start_time_list = []
                 all_resnet50_end_time_list = []
-                model = ImageNetTester(self.yaml_config)
-                model.load_model()
+                self.model = ModelFactory(self.model_info)
+                self.model.load_model()
                 request = [test_data['actual_input']]
                 start = time.time()
                 for _ in range(iterations):
                     resnet50_start_time = time.time()
                     all_resnet50_start_time_list.append(resnet50_start_time * 1000)
-                    output_data = model.inference(request)
+                    output_data = self.model.inference(request)
                     resnet50_end_time = time.time()
                     all_resnet50_end_time_list.append(resnet50_end_time * 1000)
                 start_time_list = all_resnet50_start_time_list
@@ -105,14 +103,14 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
                 test_data = dataloader.get_samples(0)
                 all_conformer_start_time_list = []
                 all_conformer_end_time_list = []
-                model = ConformerTester(self.yaml_config)
-                model.load_model()
+                self.model = ModelFactory(self.model_info)
+                self.model.load_model()
                 request = [test_data["src"], test_data["src_pad_mask"]]
                 start = time.time()
                 for _ in range(iterations):
                     conformer_start_time = time.time()
                     all_conformer_start_time_list.append(conformer_start_time * 1000)
-                    output_data = model.inference(request)
+                    output_data = self.model.inference(request)
                     conformer_end_time = time.time()
                     all_conformer_end_time_list.append(conformer_end_time * 1000)
                 start_time_list = all_conformer_start_time_list
@@ -137,20 +135,20 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
                             _preprocess_queue.put(None)
                             return
 
-                        input_data_list, info = squad_preprocess(data, model_info)
+                        input_data_list, info = self.model.preprocess(data, model_info)
                         _preprocess_queue.put(input_data_list)
                         _info_queue.put(info)
 
                 def inference_worker(_preprocess_queue, _inference_queue, config):
-                    _model = SQUADTester(config)
-                    model.load_model()
+                    self.model = ModelFactory(config)
+                    self.model.load_model()
                     while True:
                         data = _preprocess_queue.get()
                         if data is None:
                             _inference_queue.put(None)
-                            _model.destroy()
+                            self.model.destroy()
                             return
-                        _output_data = model.inference(data)
+                        _output_data = self.model.inference(data)
                         _inference_queue.put(_output_data)
 
                 def postprocessing_worker(_inference_queue, _postprocess_queue, _info_queue):
@@ -161,7 +159,7 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
                             _postprocess_queue.put(None)
                             _info_queue.put(None)
                             return
-                        _postprocess_queue.put(squad_postprocess(data, info))
+                        _postprocess_queue.put(self.model.postprocess(data, info))
 
                 def consumer(_postprocess_queue, _shared_end_list):
                     ans = []
@@ -188,10 +186,10 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
                     target=input_worker, args=(input_queue, test_data, iterations, shared_start_list))
                 # [1] 模型前处理进程
                 preprocessing_process = multiprocessing.Process(
-                    target=preprocessing_worker, args=(input_queue, preprocess_queue, info_queue, self.yaml_config))
+                    target=preprocessing_worker, args=(input_queue, preprocess_queue, info_queue, self.model_info))
                 # [2] 模型推理进程
                 inference_process = multiprocessing.Process(
-                    target=inference_worker, args=(preprocess_queue, inference_queue, self.yaml_config))
+                    target=inference_worker, args=(preprocess_queue, inference_queue, self.model_info))
 
                 # [3] 模型后处理进程
                 postprocessing_process = multiprocessing.Process(
@@ -236,7 +234,7 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
     def get_loaded_batch_size(self):
         # only used in accuracy mode, not in benchmark.
         name = self.configs['model']
-        self.yaml_config.update({"min_batch_size": self.yaml_config['chunk_size']})
+        self.model_info.update({"min_batch_size": self.model_info['chunk_size']})
         if "bert-torch-fp32" in name or "albert-torch-fp32" in name:
             return 12
         elif "roberta-torch-fp32" in name:
@@ -252,26 +250,22 @@ class RuntimeBackendSPU(runtime_backend.RuntimeBackend):
         self.batch_size = batch_size
         self.model_name = self.configs['model']
         self.model_info.update({"input_name": self.model_info['inputs'].split(",")})
-        self.yaml_config = yaml.safe_load(open(get_yaml_path(self.model_info["model"]), "r"))
-        self.yaml_config.update({
-            "model": self.configs["model"],
-            "input_name": self.model_info["input_name"]
-        })
-        if 'input_order' in self.yaml_config["model_input"][0]:
-            self.yaml_config.update(
-                {"input_order": [inp['input_order'] for inp in self.yaml_config["model_input"]]})
 
         if self.need_reload:
-            dataset_name = self.model_info['dataset_name']
-            if dataset_name == "open_squad":
-                self.model = SQUADTester(self.model_info)
-            elif dataset_name == "open_imagenet":
-                self.model = ImageNetTester(self.model_info)
-            elif dataset_name == "fake_dataset":
-                self.model = ConformerTester(self.model_info)
-            else:
-                raise NotImplementedError(f"dataset_name : {dataset_name} not supported")
+            self.model = ModelFactory(self.model_info)
             self.model.load_model()
             self.need_reload = False
         else:
             log.info("model has been loaded, skip load process")
+
+        task_name = self.model_info["model"]
+        self.model_info = yaml.safe_load(open(f"./byte_mlperf/download/moffett/converted_models/{task_name}.yaml", "r"))
+        self.model_info.update({
+            "model": self.configs["model"],
+            "input_name": self.model_info["input_name"]
+        })
+        if 'input_order' in self.model_info["model_input"][0]:
+            self.model_info.update(
+                {"input_order": [inp['input_order'] for inp in self.model_info["model_input"]]})
+
+
