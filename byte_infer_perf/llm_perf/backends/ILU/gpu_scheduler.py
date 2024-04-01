d9f76870c173882000c21017427372dc793a69ab
sen.li <sen.li@iluvatar.com>
2024-04-01 20:20:20 +0800

    author: zhihui.liao add iluvatar chatglm2 && chinese-llama2 backend.

---
+++ byte_infer_perf/llm_perf/backends/ILU/gpu_scheduler.py
@@ -0,0 +1,85 @@
+import sys
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import List
+
+import torch
+from transformers import PreTrainedTokenizer
+
+import llm_perf.backends.GPU.common as comm
+from llm_perf.core.common import Packet
+from llm_perf.core.engine import CoreEngine
+from llm_perf.core.sampler import CoreSampler
+from llm_perf.core.scheduler import CoreScheduler
+from llm_perf.utils.logger import logger
+
+
+class GpuScheduler(CoreScheduler):
+    def __init__(
+        self,
+        engine: CoreEngine,
+        sampler: CoreSampler,
+        tokenizer: PreTrainedTokenizer,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            engine=engine, sampler=sampler, tokenizer=tokenizer, comm=comm, **kwargs
+        )
+        self.max_batch_size = kwargs.get("max_batch_size")
+
+    @torch.inference_mode()
+    def scheduler_loop(self):
+        batch: List[Packet] = []
+        while True:
+            # 1. get new task
+            batch = self.select_batch(batch)
+            if not batch:
+                with self.packet_queue.not_empty:
+                    self.packet_queue.not_empty.wait(0.1)
+                continue
+
+            logger.info(f"get batch size: {len(batch)}")
+
+            # 2. do inference -> logits
+            outputs = self.engine.do_inference(batch)
+
+            logger.info(f"@@@@@@@@@@@@@@  2. do inference -> logits") 
+            # 3. sample logits -> tokens
+            next_tokens, softmax_out = self.sampler.sample(
+                packets=batch, logits=outputs["last_logits"]
+            )
+
+            logger.info(f"@@@@@@@@@@@@@@  4.postprocess -> gen result ")
+            # 4.postprocess -> gen result
+            generation_results = self.sampler.postprocess(
+                packets=batch,
+                infer_outputs=outputs,
+                next_tokens=next_tokens,
+            )
+
+            logger.info(f"@@@@@@@@@@@@@@  5. add result to packet ")
+            # 5. add result to packet
+            for i, gen_res in enumerate(generation_results):
+                batch[i].add_result(gen_res)
+                if gen_res.finish_reason:
+                    batch[i].finish()
+
+            # 6. is not finished -> remain
+            remained: List[Packet] = []
+            for packet in batch:
+                if not packet.is_finished():
+                    remained.append(packet)
+            logger.info(f"@@@@@@@@@@@@@@  6. is not finished -> remain ")        
+            batch = remained
+
+    def select_batch(self, batch):
+        batching_size: int = len(batch)
+        new_select_packets: List[Packet] = []
+
+        while not self.packet_queue.empty():
+            if batching_size == self.max_batch_size:
+                break
+            batching_size += 1
+            new_select_packets.append(self.packet_queue.get())
+
+        return batch + new_select_packets
