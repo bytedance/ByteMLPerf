1dd1d7295617601327da78af8dfbb5bc812b12c8
gaoyujia <gaoyujia.01@bytedance.com>
2024-07-18 16:03:05 +0800

    micro perf: add NPU backend

---
+++ byte_micro_perf/backends/NPU/backend_npu.py
@@ -0,0 +1,276 @@
+# Copyright 2023 ByteDance and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import logging
+import math
+import os
+from datetime import timedelta
+from typing import Any, Dict, List
+
+import torch
+import torch_npu
+import torch.distributed as dist
+import torch.distributed.distributed_c10d as dist_c10d
+
+from backends.backend import Backend
+from backends.module_store import *
+from backends.utils import get_dtype_bytes
+
+from .custom_ops import NPUGemmOp, NPUBatchGemmOp, NPUGroupGemmOp
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("PerfEngine")
+
+
+class BackendNPU(Backend):
+    def get_device_name(self):
+        return torch.npu.get_device_name(0)
+
+    def get_backend_properties(self):
+        self.memory_limit = int(
+            torch.npu.get_device_properties(0).total_memory / (1024**3)
+        )
+
+        if os.path.exists(self.vendor_path) and (self.vendor_path).endswith(".json"):
+            with open(self.vendor_path, "r") as f:
+                self.hw_info_dict = json.load(f)
+                # if the vendor path does not exist, please set this param manaually
+                self.bandwidth_limit = self.hw_info_dict["内存参数"]["内存"]["内存带宽(GB/s)"]
+        else:
+            log.warning(
+                "Vendor_path: [ {} ] was not found or not a full path points to json, please check your path!!! Otherwise, please set the hardware info manaually.".format(
+                    self.vendor_path
+                )
+            )
+
+
+    # device/host ops
+    def host2device(self):
+        self.op = Host2DeviceOp(torch.device("npu"))
+
+    def device2host(self):
+        self.op = Device2HostOp()
+
+
+    # communication ops
+    def allreduce(self):
+        self.setup_2d_group()
+        self.op = AllReduceOp(self.group)
+
+    def allgather(self):
+        self.setup_2d_group()
+        self.op = AllGatherOp(self.group)
+
+    def reducescatter(self):
+        self.setup_2d_group()
+        self.op = ReduceScatterOp(self.group)
+
+    def alltoall(self):
+        self.setup_2d_group()
+        self.op = AllToAllOp(self.group)
+
+    def broadcast(self):
+        self.setup_2d_group()
+        self.op = BroadcastOp(self.group)
+
+    def p2p(self):
+        self.setup_2d_group()
+        self.op = P2POp(self.group, self.ranks, self.rank)
+
+    # compute ops
+    # unary ops
+    def sin(self):
+        self.op = SinOp()
+
+    def cos(self):
+        self.op = CosOp()
+
+    def exp(self):
+        self.op = ExpOp()
+
+    def exponential(self):
+        self.op = ExponentialOp()
+
+    def silu(self):
+        self.op = SiluOp()
+
+    def gelu(self):
+        self.op = GeluOp()
+
+    def swiglu(self):
+        self.op = SwiGLUOp()
+
+    def cast(self):
+        self.op = CastOp()
+
+
+    # binary ops
+    def add(self):
+        self.op = AddOp()
+
+    def mul(self):
+        self.op = MulOp()
+
+    def sub(self):
+        self.op = SubOp()
+
+    def div(self):
+        self.op = DivOp()
+
+
+    # reduce ops
+    def layernorm(self):
+        self.op = LayerNormOp()
+
+    def softmax(self):
+        self.op = SoftmaxOp()
+
+    def reduce_sum(self):
+        self.op = ReduceSumOp()
+
+    def reduce_min(self):
+        self.op = ReduceMinOp()
+
+    def reduce_max(self):
+        self.op = ReduceMaxOp()
+
+
+    # index ops
+    def index_add(self):
+        self.op = IndexAddOp()
+
+    def sort(self):
+        self.op = SortOp()
+
+    def unique(self):
+        self.op = UniqueOp()
+
+
+    # gemm ops
+    def gemm(self):
+        self.op = NPUGemmOp()
+
+    def gemv(self):
+        self.op = NPUGemmOp()
+
+    def batch_gemm(self):
+        self.op = NPUBatchGemmOp()
+
+    def group_gemm(self):
+        # self.op = NPUGroupGemmOp()
+        self.op = GroupGemmOp()
+
+    # create input tensors
+    def build_tensor(self, input_shapes, dtype):
+        torch.npu.empty_cache()
+        torch_dtype = getattr(torch, dtype)
+
+        # compute size of input and output tensors
+        if hasattr(self.op, "compute_size"):
+            bytes_per_cnt = self.op.compute_size(input_shapes, dtype)
+        # default: input_tensors_size == output_tensor_size, all tensors have same dtype
+        else:
+            dtype_size = get_dtype_bytes(dtype)
+            element_num = 2 * sum([math.prod(shape) for shape in input_shapes])
+            bytes_per_cnt = dtype_size * element_num
+
+        # compute max avail tensors for compute
+        avail_bytes = (self.memory_limit - 4) * 1024**3
+        avail_cnts = avail_bytes // bytes_per_cnt
+        max_data_cnt = min(self.iterations, avail_cnts)
+
+        # create input tensors for each op
+        input_tensors_list = []
+        for _ in range(max_data_cnt):
+            # create input tensors
+            if hasattr(self.op, "custom_create_tensors"):
+                input_tensors = self.op.custom_create_tensors(input_shapes, torch_dtype, "npu")
+                input_tensors_list.append(input_tensors)
+            # default: all input tensors have same dtype
+            else:
+                if torch_dtype in [torch.int8, torch.int32]:
+                    input_tensors = [
+                        torch.randint(-3, 3, size=shape, dtype=torch_dtype, device="npu")
+                        for shape in input_shapes
+                    ]
+                else:
+                    input_tensors = [
+                        torch.randn(shape, dtype=torch_dtype, device="npu")
+                        for shape in input_shapes
+                    ]
+                input_tensors_list.append(input_tensors)
+        if hasattr(self.op, "process_inputs"):
+            input_tensors_list = [
+                self.op.process_inputs(*(input_tensor))
+                for input_tensor in input_tensors_list
+            ]
+        return input_tensors_list, max_data_cnt, bytes_per_cnt
+
+
+
+    def _run_operation(self, operation, inputs):
+        result = operation(*inputs)
+        return result
+
+    def device_synchronize(self):
+        torch.npu.synchronize()
+        return True
+
+    def initialize_ccl(self, rank, world_size):
+        """
+        initialize distributed process groups and relevant ENVs
+        """
+        # check device_count
+        device_count = torch.npu.device_count()
+        if world_size > device_count:
+            world_size = device_count
+        if rank >= world_size:
+            return False
+
+        # set envs
+        os.environ["MASTER_ADDR"] = "127.0.0.1"
+        os.environ["MASTER_PORT"] = "49373"
+        os.environ["LOCAL_RANK"] = str(rank)
+        os.environ["RANK"] = str(rank)
+        os.environ["WORLD_SIZE"] = str(world_size)
+
+        torch.npu.set_device(rank)
+
+        # Call the init process
+        timeout_seconds = int(os.environ.get("MEGATRON_NCCL_TIMEOUT_SECOND", 30))
+        torch.distributed.init_process_group(
+            backend="nccl",
+            world_size=world_size,
+            rank=rank,
+            store=None,
+            timeout=timedelta(seconds=timeout_seconds),
+        )
+        self.setup_2d_group()
+        log.warning("DIST: rank {}, world_size {}".format(rank, world_size))
+        return True
+
+    def setup_2d_group(self):
+        self.rank = dist.get_rank()
+        torch.npu.set_device(self.rank)
+        origin_store_based_barrier = dist_c10d._store_based_barrier
+        dist_c10d._store_based_barrier = lambda *a, **kw: None
+        self.world_size = dist.get_world_size()
+        self.ranks = range(0, self.world_size)
+        group = dist.new_group(self.ranks)
+        if self.rank in self.ranks:
+            self.group = group
+        dist_c10d._store_based_barrier = origin_store_based_barrier
+        # wait for all ranks finish group initializing
+        torch.distributed.barrier()
---
+++ byte_micro_perf/backends/NPU/custom_ops.py
@@ -0,0 +1,154 @@
+from typing import List
+
+import torch
+import torch_npu
+import math
+
+from backends.module_store import GemmOp, BatchGemmOp, GroupGemmOp
+from backends.utils import get_dtype_bytes
+
+
+# gemm(pytorch)   float32/float16/bfloat16 --> float32/float16/bfloat16
+# gemm(torch_npu) int8 --> int8
+class NPUGemmOp(GemmOp):
+    def __init__(self):
+        super().__init__()
+        scale = torch.randn(16, dtype=torch.float32)
+
+    def compute_size(self, input_shapes, dtype):
+        # input_shapes: [[M, K], [K, N]]
+        torch_dtype = getattr(torch, dtype)
+        a_shape, b_shape = input_shapes
+        M, K = a_shape
+        K, N = b_shape
+        d_shape = [M, N]
+        dtype_size = get_dtype_bytes(dtype)
+        input_element_num = sum([math.prod(shape) for shape in [a_shape, b_shape]])
+        output_element_num = sum([math.prod(shape) for shape in [d_shape]])
+        if torch_dtype == torch.int8:
+            bytes_per_cnt = dtype_size * input_element_num + get_dtype_bytes("float32") * (output_element_num + N)
+        else:
+            bytes_per_cnt = dtype_size * (input_element_num + output_element_num)
+        return bytes_per_cnt
+
+    def custom_create_tensors(self, input_shapes, torch_dtype, xpu_device):
+        """
+        [
+            [[M1, K1], [K1, N1]], 
+            [[M2, K2], [K2, N2]]
+        ]
+        """
+        if torch_dtype in [torch.int8, torch.int32]:
+            input_tensors = [
+                torch.randint(-3, 3, size=shape, dtype=torch_dtype, device=xpu_device)
+                for shape in input_shapes
+            ]
+        else:
+            input_tensors = [
+                torch.randn(shape, dtype=torch_dtype, device=xpu_device)
+                for shape in input_shapes
+            ]
+        if torch_dtype in [torch.int8]:
+            N = input_shapes[1][1]
+            input_tensors.append(torch.randn([N], dtype=torch.float32, device=xpu_device))
+        else:
+            input_tensors.append(None)
+
+        return input_tensors
+
+    def forward(
+        self, 
+        input_tensor_a : torch.Tensor, 
+        input_tensor_b : torch.Tensor,
+        scale = None
+    ):
+        compute_dtype = input_tensor_a.dtype
+        if compute_dtype == torch.int8:
+            output_tensor = torch_npu.npu_quant_matmul(input_tensor_a, input_tensor_b, scale)
+        else:
+            output_tensor = torch.mm(input_tensor_a, input_tensor_b)
+        return output_tensor
+
+
+# batch_gemm(pytorch)     float32/float16/bfloat16 --> float32/float16/bfloat16
+# batch_gemm(torch_npu)   int8 --> int8
+class NPUBatchGemmOp(BatchGemmOp):
+    def __init__(self):
+        super().__init__()
+
+    def compute_size(self, input_shapes, dtype):
+        # input_shapes: [[bs, M, K], [bs, K, N]]
+        torch_dtype = getattr(torch, dtype)
+        a_shape, b_shape = input_shapes
+        bs, M, K = a_shape
+        bs, K, N = b_shape
+        d_shape = [bs, M, N]
+        dtype_size = get_dtype_bytes(dtype)
+        input_element_num = sum([math.prod(shape) for shape in [a_shape, b_shape]])
+        output_element_num = sum([math.prod(shape) for shape in [d_shape]])
+        if torch_dtype == torch.int8:
+            bytes_per_cnt = dtype_size * input_element_num + get_dtype_bytes("int32") * (output_element_num + N)
+        else:
+            bytes_per_cnt = dtype_size * (input_element_num + output_element_num)
+        return bytes_per_cnt
+
+    def custom_create_tensors(self, input_shapes, torch_dtype, xpu_device):
+        """
+        [
+            [[M1, K1], [K1, N1]], 
+            [[M2, K2], [K2, N2]]
+        ]
+        """
+        if torch_dtype in [torch.int8, torch.int32]:
+            input_tensors = [
+                torch.randint(-3, 3, size=shape, dtype=torch_dtype, device=xpu_device)
+                for shape in input_shapes
+            ]
+        else:
+            input_tensors = [
+                torch.randn(shape, dtype=torch_dtype, device=xpu_device)
+                for shape in input_shapes
+            ]
+        if torch_dtype in [torch.int8]:
+            N = input_shapes[1][2]
+            input_tensors.append(torch.randn([N], dtype=torch.float32, device=xpu_device))
+        else:
+            input_tensors.append(None)
+
+        return input_tensors
+    
+
+    def forward(
+        self, 
+        input_tensor_a : torch.Tensor, 
+        input_tensor_b : torch.Tensor,
+        scale = None
+    ):
+        compute_dtype = input_tensor_a.dtype
+
+        output_tensor = None
+        if compute_dtype == torch.int8:
+            output_tensor = torch_npu.npu_quant_matmul(input_tensor_a, input_tensor_b, scale)
+        else:
+            output_tensor = torch.bmm(input_tensor_a, input_tensor_b)
+        return output_tensor
+
+
+# group_gemm(pytorch)   float32--> float32
+# batch_gemm(torch_npu) float16/bfloat16/int8 --> float16/bfloat16/int8
+class NPUGroupGemmOp(GroupGemmOp):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, 
+        a_list : List[torch.Tensor], 
+        b_list : List[torch.Tensor]
+    ):
+        compute_dtype = a_list[0].dtype
+        if compute_dtype == torch.int8:
+            output_tensors = torch_npu.npu_grouped_matmul(a_list, b_list)
+        elif compute_dtype == torch.float32:
+            output_tensors = [a @ b for a, b in zip(a_list, b_list)]
+        else:
+            output_tensors = torch_npu.npu_grouped_matmul(a_list, b_list)
+        return output_tensors
