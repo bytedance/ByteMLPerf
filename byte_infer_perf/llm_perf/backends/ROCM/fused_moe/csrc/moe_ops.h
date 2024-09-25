6d95e31388b6fb6a5fa81522dba7a5538f11eee3
valarLip <340077269@qq.com>
2024-09-25 21:22:51 +0800

    add rocm backend

---
+++ byte_infer_perf/llm_perf/backends/ROCM/fused_moe/csrc/moe_ops.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <torch/extension.h>
+
+void topk_softmax(torch::Tensor& topk_weights, torch::Tensor& topk_indices,
+                  torch::Tensor& token_expert_indices,
+                  torch::Tensor& gating_output);
+
+void moe_align_block_size(torch::Tensor topk_ids, int num_experts,
+                          int block_size, torch::Tensor sorted_token_ids,
+                          torch::Tensor experts_ids,
+                          torch::Tensor num_tokens_post_pad);
+void silu_and_mul(torch::Tensor &out, torch::Tensor &input);
