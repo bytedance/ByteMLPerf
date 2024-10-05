694c9c6a8f75378cb12e9d6376e614dc4be7b99b
valarLip <340077269@qq.com>
2024-10-05 22:50:16 +0800

    add fused_rope

--- byte_infer_perf/llm_perf/backends/ROCM/rocm_kernels/csrc/moe_ops.h
+++ byte_infer_perf/llm_perf/backends/ROCM/rocm_kernels/csrc/moe_ops.h
@@ -21,3 +21,13 @@ void fused_add_rms_norm(torch::Tensor &input, torch::Tensor &residual,
 
 void wvSpltK(at::Tensor &in_a, at::Tensor &in_b, at::Tensor &out_c,
              const int64_t N_in, const int64_t CuCount);
+
+void rotary_embedding(torch::Tensor &positions, torch::Tensor &query,
+                      torch::Tensor &key, int64_t head_size,
+                      torch::Tensor &cos_sin_cache, bool is_neox);
+
+void batched_rotary_embedding(torch::Tensor &positions, torch::Tensor &query,
+                              torch::Tensor &key, int64_t head_size,
+                              torch::Tensor &cos_sin_cache, bool is_neox,
+                              int64_t rot_dim,
+                              torch::Tensor &cos_sin_cache_offsets);
