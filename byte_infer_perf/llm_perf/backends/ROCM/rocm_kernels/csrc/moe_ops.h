246420683957b10fd74fa61853ed0438d46b371d
valarLip <340077269@qq.com>
2024-10-15 14:27:36 +0800

    add missing custom_ar code...

--- byte_infer_perf/llm_perf/backends/ROCM/rocm_kernels/csrc/moe_ops.h
+++ byte_infer_perf/llm_perf/backends/ROCM/rocm_kernels/csrc/moe_ops.h
@@ -33,3 +33,26 @@ void batched_rotary_embedding(torch::Tensor &positions, torch::Tensor &query,
                               torch::Tensor &cos_sin_cache_offsets);
 
 void moe_sum(torch::Tensor &input, torch::Tensor &output);
+
+// all reduce
+using fptr_t = int64_t;
+fptr_t init_custom_ar(torch::Tensor &meta, torch::Tensor &rank_data,
+                      const std::vector<std::string> &handles,
+                      const std::vector<int64_t> &offsets, int64_t rank,
+                      bool full_nvlink);
+void all_reduce_reg(fptr_t _fa, torch::Tensor &inp, torch::Tensor &out);
+void all_reduce_unreg(fptr_t _fa, torch::Tensor &inp, torch::Tensor &reg_buffer,
+                      torch::Tensor &out);
+void dispose(fptr_t _fa);
+int64_t meta_size();
+void register_buffer(fptr_t _fa, torch::Tensor &t,
+                     const std::vector<std::string> &handles,
+                     const std::vector<int64_t> &offsets);
+std::tuple<torch::Tensor, std::vector<int64_t>> get_graph_buffer_ipc_meta(
+    fptr_t _fa);
+void register_graph_buffers(fptr_t _fa, const std::vector<std::string> &handles,
+                            const std::vector<std::vector<int64_t>> &offsets);
+#ifdef USE_ROCM
+torch::Tensor allocate_meta_buffer(int64_t size);
+torch::Tensor get_meta_buffer_ipc_handle(torch::Tensor &inp);
+#endif
--- byte_infer_perf/llm_perf/backends/ROCM/rocm_kernels/csrc/rocm_ops.cpp
+++ byte_infer_perf/llm_perf/backends/ROCM/rocm_kernels/csrc/rocm_ops.cpp
@@ -76,4 +76,30 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
       m.def("convert_fp8", &convert_fp8,
             "convert_fp8(Tensor! dst_cache, Tensor src_cache, float scale, "
             "str kv_cache_dtype) -> ()");
+
+      // Custom all-reduce kernels
+      m.def("init_custom_ar", &init_custom_ar,
+            "init_custom_ar(Tensor meta, Tensor rank_data, "
+            "str[] handles, int[] offsets, int rank, "
+            "bool full_nvlink) -> int");
+
+      m.def("all_reduce_reg", &all_reduce_reg, "all_reduce_reg(int fa, Tensor inp, Tensor! out) -> ()");
+
+      m.def("all_reduce_unreg", &all_reduce_unreg,
+            "all_reduce_unreg(int fa, Tensor inp, Tensor reg_buffer, Tensor! out) -> "
+            "()");
+
+      m.def("dispose", &dispose);
+      m.def("meta_size", &meta_size);
+
+      m.def("register_buffer", &register_buffer,
+            "register_buffer(int fa, Tensor t, str[] handles, "
+            "int[] offsets) -> ()");
+
+      m.def("get_graph_buffer_ipc_meta", &get_graph_buffer_ipc_meta);
+      m.def("register_graph_buffers", &register_graph_buffers);
+#ifdef USE_ROCM
+      m.def("allocate_meta_buffer", &allocate_meta_buffer);
+      m.def("get_meta_buffer_ipc_handle", &get_meta_buffer_ipc_handle);
+#endif
 }
