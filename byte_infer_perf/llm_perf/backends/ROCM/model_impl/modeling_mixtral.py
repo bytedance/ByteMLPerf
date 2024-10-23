0c3fde32bd2702e46127f884fb9c6969e2f07628
valarLip <340077269@qq.com>
2024-10-23 13:50:15 +0800

    add more MoE tuned config

--- byte_infer_perf/llm_perf/backends/ROCM/model_impl/modeling_mixtral.py
+++ byte_infer_perf/llm_perf/backends/ROCM/model_impl/modeling_mixtral.py
@@ -813,9 +813,6 @@ class MixtralSdpaAttention(MixtralAttention):
 
         # fused rope
         query_states, key_states = self.rotary_emb_fused(position_ids, query_states, key_states)
-        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
-        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
         # # old rope
         # cos, sin = self.rotary_emb(value_states, seq_len=max_kv_len)
         # query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
@@ -833,6 +830,9 @@ class MixtralSdpaAttention(MixtralAttention):
                                             "auto", 1.0, 1.0)
         
         if is_context:
+            query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+            key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+            value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
             key_states = repeat_kv(key_states, self.num_key_value_groups)
             value_states = repeat_kv(value_states, self.num_key_value_groups)
         
