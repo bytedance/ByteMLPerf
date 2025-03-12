3e8ad7f1e57d198537f80e54de36f22d3730ad8a
Huang Hantao <huanghantao@gmail.com>
2025-03-12 09:37:27 +0000

    diable jit

--- byte_train_perf/Megatron-LM/megatron/core/jit.py
+++ byte_train_perf/Megatron-LM/megatron/core/jit.py
@@ -4,7 +4,15 @@ import torch
 
 from megatron.core.utils import is_torch_min_version
 
-jit_fuser = torch.jit.script
+def fake_torch_compile(func):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
+
+# GPU & MLU torch compile is disabled, for precision testing.
+#jit_fuser = torch.jit.script
 # nvFuser is deprecated in PyTorch JIT starting from 2.2
-if is_torch_min_version("2.2.0a0"):
-    jit_fuser = torch.compile
+#if is_torch_min_version("2.2.0a0"):
+#    jit_fuser = torch.compile
+jit_fuser = fake_torch_compile
+
